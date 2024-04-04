1d92035b9376892633f1fee35e61560686892ec0
Ashish <ashishnangare1967@gmail.com>
2024-04-04 23:00:24 +0530

    fix

--- src/component/CustomerDetails.tsx
+++ src/component/CustomerDetails.tsx
@@ -25,7 +25,7 @@ const CustomerDetails: React.FC<CustomerDetailsProps> = ({ customer, photos }) =
         <>
           <h2>{customer.name}</h2>
           <p>{customer.title}</p>
-          <p>{customer.address}</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis cupiditate, illum, earum atque dolorem fugiat a quidem nam voluptatem ullam modi qui velit molestias quod quos non dolores quibusdam facilis.{customer.address}</p>
           <div className="photo-grid">
             {photos.map((photo) => (
               <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title} />
